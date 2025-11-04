ad3fb6f21588956606c677b3f0257ae3f76a53ff
StanislavSolovei <edukacjateb176@gmail.com>
2025-11-04 10:35:38 +0100

    Enhance analizeText function for text analysis
    Added functionality to trim whitespace and count words.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function analizeText() {
     const myText = document.getElementById("text").value;
+    
     const myTextResult = myText.length;
     document.getElementById("textResult").textContent = myTextResult;
+    
+    const textTrimmed = myText.trim();
+    document.getElementById("textResultNotSpace").textContent = textTrimmed.length;
+    
     const words = myText.split(" ");
-    document.getElementById("textResultNotSpace").textContent = words.length;
+    document.getElementById("textWords").textContent = words.length;
 };
